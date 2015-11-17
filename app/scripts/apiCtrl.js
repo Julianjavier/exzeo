263338488cc4ae2d15762145952a25d2257ee806
Julian Rodriguez <greyfox893@gmail.com>
2015-11-16 21:53:58 -0400

    Reworked filter to pertain only to the top 100 entries.

--- app/scripts/apiCtrl.js
+++ app/scripts/apiCtrl.js
@@ -1,4 +1,7 @@
 exzeo.controller('apiCtrl', ['$scope', '$rootScope','$http', '$location', 'Pagination', function($scope, $rootScope, $http, $location, Pagination){
+	//variable to be used to show or hide the proper filtered entries
+	$scope.filter = false
+
 	$http.get('https://itunes.apple.com/us/rss/topalbums/limit=100/json')
   	.success(function(apiData){
 			//Checks to make sure data exists.
@@ -10,18 +13,19 @@ exzeo.controller('apiCtrl', ['$scope', '$rootScope','$http', '$location', 'Pagin
 				//iterator to remove duplicate entries in the select list.
 				var iterator = [];
 
+				//sets the fist call from the api
 				$scope.itunesList = apiData.feed.entry;
+				//sets default variable for the select list
 				$scope.selectVar = $scope.list[0];
 
+				//sets the values for the categories only found in the top 100
 				angular.forEach(apiData.feed.entry, function(value) {
 					var key = value.category.attributes["im:id"]
-
 					if(iterator.indexOf(key) === -1) {
 							iterator.push(key);
 							list.push({id:key , label:value.category.attributes.label});
 					}
 				});
-				console.log(list)
 
     	}else{
     		console.log("we got nothing");
@@ -32,12 +36,18 @@ exzeo.controller('apiCtrl', ['$scope', '$rootScope','$http', '$location', 'Pagin
 	      console.log('NONO ',apiData);
 	  });
 
-		$scope.submit = function(){
-			var category = $scope.selectVar.id;
-			if (category) {
-				console.log(category);
-				$location.path('/filter/'+category);
-			};
+		$scope.submit = function(value){
+			$scope.filter = true;
+			$scope.filteredList = [];
+			var gener = $scope.selectVar.label;
+			angular.forEach($scope.itunesList, function(value, key) {
+				if (value.category.attributes.label === gener) {
+					$scope.filteredList.push(value)
+				}
+			});
+
+			console.log($scope.filteredList);
+
 		};
 
 }]);
